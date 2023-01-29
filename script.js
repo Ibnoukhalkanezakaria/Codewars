f5e37fa8bfefbc16509b3539cffbda195d07af03
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-01-29 17:22:08 +0100

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+// function multiply(a, b) {
+//   return a * b;
+// }
+// console.log(multiply);
+
+// https://www.codewars.com/kata/515e271a311df0350d00000f
+// function squareSum(numbers) {
+//   let count = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     count += numbers[i] ** 2;
+//   }
+//   return count;
+// }
