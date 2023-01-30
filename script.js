67aac1524fba2cef22add468f143111d810fe82b
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-01-30 13:08:43 +0100

    new

--- script.js
+++ script.js
@@ -130,3 +130,14 @@
 //     return number * 9;
 //   }
 // }
+
+// https://www.codewars.com/kata/57e3f79c9cb119374600046b/train/javascript
+// function hello(name) {
+//   if (name) {
+//     return `Hello, ${name.charAt(0).toUpperCase()}${name
+//       .slice(1)
+//       .toLowerCase()}!`;
+//   } else {
+//     return `Hello, World!`;
+//   }
+// }
